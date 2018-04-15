359df2e8a6da9fb57945f84334f9a92dcc1bd3fd
Marcus Kida <marcus@kida.io>
2018-04-15 20:25:35 +0200

    Initial Commit

---
+++ HackerSaver/HackerSaverView.h
@@ -0,0 +1,13 @@
+//
+//  HackerSaverView.h
+//  HackerSaver
+//
+//  Created by Marcus Kida on 15.04.18.
+//  Copyright © 2018 Marcus Kida. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface HackerSaverView : ScreenSaverView
+
+@end
---
+++ HackerSaver/HackerSaverView.m
@@ -0,0 +1,52 @@
+//
+//  HackerSaverView.m
+//  HackerSaver
+//
+//  Created by Marcus Kida on 15.04.18.
+//  Copyright © 2018 Marcus Kida. All rights reserved.
+//
+
+#import "HackerSaverView.h"
+
+@implementation HackerSaverView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
